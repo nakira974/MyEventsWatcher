6795a32ee57e2f5e47897ce8658144cf907f5f2c
nakira974 <maxime.loukhal@gmail.com>
2022-05-20 16:41:58 +0200

    Correct Venue.cs

--- MyEventsWatcher.Shared/Models/Orion/Venue.cs
+++ MyEventsWatcher.Shared/Models/Orion/Venue.cs
@@ -17,7 +17,7 @@ namespace MyEventsWatcher.Shared.Models.Orion
     public record Aliases
     {
         [JsonPropertyName("value")]
-        public List<string> AlliasesValue { get; set; }
+        public List<string>? AlliasesValue { get; set; }
     }
 
     public record ChildrenRule
